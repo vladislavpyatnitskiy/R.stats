e0ef1ea443ac8e2aad09adf77ec1bb9b6e4f28c3
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-08 14:23:16 +0500

    Update Poisson Distribution.R
    Substituted PROD(SEQ) with FACTORIAL

--- Poisson Distribution.R
+++ Poisson Distribution.R
@@ -1,7 +1,5 @@
 poisson.distribution <- function(x, y){ # Poisson Distribution
   
-  if (y == 0){ x ^ y * exp(-x) / prod(seq(1)) # 0! = 1
-  
-    } else { x ^ y * exp(-x) / prod(seq(y)) }
+  x ^ y * exp(-x) / factorial(y)
 }
 poisson.distribution(3, 1) # Test
