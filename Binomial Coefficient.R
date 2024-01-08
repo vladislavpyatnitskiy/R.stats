a3647809d6b0a3d6aeba3f303034f84cf18fc32c
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-08 14:09:48 +0500

    Update Binomial Coefficient.R
    Substituted prod(seq) combination to factorial

--- Binomial Coefficient.R
+++ Binomial Coefficient.R
@@ -1,5 +1,5 @@
 binom.coefficient <- function(x, y){ # Binomial Coefficient
   
-  prod(seq(x)) / (prod(seq(y)) * prod(seq(x - y)))
+  factorial(x) / (factorial(y) * factorial(x - y))
 }
 binom.coefficient(59,6) # Test
