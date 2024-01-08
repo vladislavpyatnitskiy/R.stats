0432cb2c3cd8e0a11b9921c10ac601764e392a17
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-08 14:12:44 +0500

    Update Binomial Distribution.R
    Substituted prod(seq) with factorial

--- Binomial Distribution.R
+++ Binomial Distribution.R
@@ -2,10 +2,10 @@ binomial.distribution <- function(x, y, p){ # Binomial Distribution
   
   if (y == 0){ # When y = 0 => 0! = 1
     
-    prod(seq(x)) / (prod(seq(1)) * prod(seq(x - y))) * p ^ y * (1 - p)^(x - y)
+    factorial(x) / (factorial(1) * factorial(x - y)) * p^y * (1 - p)^(x - y)
     
   } else { # For other cases
-  
-  prod(seq(x)) / (prod(seq(y)) * prod(seq(x - y))) * p ^ y * (1 - p) ^ (x - y)} 
+    
+    factorial(x) / (factorial(y) * factorial(x - y)) * p^y * (1 - p)^(x - y) } 
 }
 binomial.distribution(4,0,.1) # Test
