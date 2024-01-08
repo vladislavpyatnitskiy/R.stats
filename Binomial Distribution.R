bea518c4a360b14b8521e6e00ca84c02c21d476f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-08 14:20:45 +0500

    Update Binomial Distribution.R
    Reduced excessive lines as factorial function does not need IF statements to reflect 0! comparing to prod(seq)

--- Binomial Distribution.R
+++ Binomial Distribution.R
@@ -1,11 +1,5 @@
 binomial.distribution <- function(x, y, p){ # Binomial Distribution
   
-  if (y == 0){ # When y = 0 => 0! = 1
-    
-    factorial(x) / (factorial(1) * factorial(x - y)) * p^y * (1 - p)^(x - y)
-    
-  } else { # For other cases
-    
-    factorial(x) / (factorial(y) * factorial(x - y)) * p^y * (1 - p)^(x - y) } 
+  factorial(x) / (factorial(y) * factorial(x - y)) * p ^ y * (1 - p) ^ (x - y) 
 }
 binomial.distribution(4,0,.1) # Test
