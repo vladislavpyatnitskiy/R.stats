3331ab4eb9bd9a51fcbef4a9a56e7ed3ff2da5b8
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-10-20 19:41:16 +0500

    Update Binomial Distribution.R
    Added option to get probability of several cases

--- Binomial Distribution.R
+++ Binomial Distribution.R
@@ -1,5 +1,5 @@
 binomial.distribution <- function(x, y, p){ # Binomial Distribution
   
-  factorial(x) / (factorial(y) * factorial(x - y)) * p ^ y * (1 - p) ^ (x - y) 
+  sum(factorial(x) / (factorial(y) * factorial(x - y)) * p^y * (1 - p)^(x - y))
 }
 binomial.distribution(4,0,.1) # Test
