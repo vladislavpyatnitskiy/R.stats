72d0f8b3548d752eb376a86717e27af1e5f24435
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-10-21 11:35:25 +0500

    Update Poisson Distribution.R
    Added option to calculate probability for several events

--- Poisson Distribution.R
+++ Poisson Distribution.R
@@ -1,5 +1,5 @@
 poisson.distribution <- function(x, y){ # Poisson Distribution
-  
-  x ^ y * exp(-x) / factorial(y)
+    
+  sum(x ^ y * exp(-x) / factorial(y))
 }
 poisson.distribution(3, 1) # Test
