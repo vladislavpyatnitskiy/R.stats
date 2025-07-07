facc5db9cd70d8a477f246d2aa24f3ec272b3a61
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-07 12:05:08 +0500

    Update Negative Binomial Distribution.R
    Optimised script:
    
    • Eliminated ELSE statement by applying RETURN in IF statement

--- Negative Binomial Distribution.R
+++ Negative Binomial Distribution.R
@@ -1,7 +1,7 @@
 binomial.distribution.negative <- function(x, y, p){ # Negative Binomial
   
-  if (y == 0){ print("Use Geometric Distriubtion Please") } else {
-  
-  factorial(x-1) / (factorial(y-1) * factorial(x-y-1)) * p^y * (1-p)^(x-y) }
+  if (y == 0) return("Please Use Geometric Distriubtion Instead") 
+    
+  factorial(x-1) / (factorial(y-1) * factorial(x-y-1)) * p^y * (1-p)^(x-y) 
 }
 binomial.distribution.negative(4, 1, .9) # Test
